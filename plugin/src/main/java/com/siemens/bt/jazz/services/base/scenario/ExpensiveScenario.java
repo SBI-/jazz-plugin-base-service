93625b902ee6ca46a081b5feb842888c131af99c
SBI- <pascal.huerlimann@gmail.com>
2017-12-01 14:15:50 +0100

    Parse response xml

--- plugin/src/main/java/com/siemens/bt/jazz/services/base/scenario/ExpensiveScenario.java
+++ plugin/src/main/java/com/siemens/bt/jazz/services/base/scenario/ExpensiveScenario.java
@@ -8,7 +8,12 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -16,7 +21,7 @@ import java.util.ArrayList;
 public class ExpensiveScenario implements AutoCloseable {
     CloseableHttpClient client = HttpClients.createDefault();
 
-    public ExpensiveScenario(String name) throws IOException {
+    public ExpensiveScenario(String name) throws IOException, ParserConfigurationException, SAXException {
         // post scenario name to start service
         HttpPost post = new HttpPost("https://localhost:9443/ccm/service/com.ibm.team.repository.service.serviceability.IScenarioRestService/scenarios/startscenario");
         post.setHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -28,6 +33,12 @@ public class ExpensiveScenario implements AutoCloseable {
         // keep the response for closing the session again
         CloseableHttpResponse execute = client.execute(post);
         HttpEntity result = execute.getEntity();
+
+        // extract data from xml
+        Document document = DocumentBuilderFactory
+                .newInstance()
+                .newDocumentBuilder()
+                .parse(result.getContent());
     }
 
     @Override
