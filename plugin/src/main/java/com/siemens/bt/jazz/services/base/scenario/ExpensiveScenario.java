32754b2ba880a67921a9ee854229f3c21ea3e982
SBI- <pascal.huerlimann@gmail.com>
2017-12-01 14:08:20 +0100

    Add primitive post implementation

--- plugin/src/main/java/com/siemens/bt/jazz/services/base/scenario/ExpensiveScenario.java
+++ plugin/src/main/java/com/siemens/bt/jazz/services/base/scenario/ExpensiveScenario.java
@@ -1,11 +1,37 @@
 package com.siemens.bt.jazz.services.base.scenario;
 
-public class ExpensiveScenario {
-    public void start() {
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+public class ExpensiveScenario implements AutoCloseable {
+    CloseableHttpClient client = HttpClients.createDefault();
+
+    public ExpensiveScenario(String name) throws IOException {
+        // post scenario name to start service
+        HttpPost post = new HttpPost("https://localhost:9443/ccm/service/com.ibm.team.repository.service.serviceability.IScenarioRestService/scenarios/startscenario");
+        post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        ArrayList<NameValuePair> body = new ArrayList<>();
+        body.add(new BasicNameValuePair("scenarioName", name));
+        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(body);
+        post.setEntity(entity);
+
+        // keep the response for closing the session again
+        CloseableHttpResponse execute = client.execute(post);
+        HttpEntity result = execute.getEntity();
     }
 
-    public void stop() {
-        
+    @Override
+    public void close() throws Exception {
+        // send stop to service
     }
 }
